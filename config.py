4e7d0d974cd8bcd67f48ebca05ece044fc73d735
luantriescode <luantriescode@gmail.com>
2026-03-27 23:13:52 +0700

    fixtimeoff

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ ROI_W, ROI_H = 120, 80
 
 # --- DETECTION THRESHOLDS (Cần thiết cho MotionDetector) ---
 MOTION_THRESHOLD = 500   # Ngưỡng diện tích chuyển động
-PERSON_LOST_DELAY = 10   # Thời gian chờ tắt đèn (giây)
+PERSON_LOST_DELAY = 3   # Thời gian chờ tắt đèn (giây)
 
 # --- NETWORK ---
 WEB_HOST = os.getenv("WEB_HOST", "192.168.1.8")
